fb63e0be02a123c503fbfbfba09abeb153f672d1
mo <13930153501@163.com>
2022-03-31 03:19:20 +0800

    添加配置文件自动加载示例

--- mybatis/src/main/java/com/codedream/mybatis/MybatisConfig.java
+++ mybatis/src/main/java/com/codedream/mybatis/MybatisConfig.java
@@ -1,5 +1,6 @@
 package com.codedream.mybatis;
 
+import com.codedream.auto.properties.PropertySource;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
  *
  * @author mo
  */
+@PropertySource(value = "classpath:/codedream-mybatis.yml")
 @Configuration(proxyBeanMethods = false)
 public class MybatisConfig {
 
